44cb9b2ea9df8cc7a87e48164b9ae926a2e16810
Isaque Lopes <isaque.lopes047@gmail.com>
2022-09-17 09:16:26 -0300

    add

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import app from './src/app.js'
-const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-})
-console.log(`${port}`)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
